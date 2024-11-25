0700cce9ef85594916596859e594cfacc1c35def
Mateo Pikl <piklmateo07@gmail.com>
2024-11-25 15:03:47 +0100

    Implemented member list and member detail pages

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -9,25 +9,11 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
-  http = inject(HttpClient);
+export class HomeComponent {
   registerMode = false;
-  users: any;
-
-  ngOnInit(): void {
-    this.getUsers();
-  }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
-  }
-
-  getUsers() {
-    this.http.get("http://localhost:5000/api/users").subscribe({
-      next: response => this.users = response,
-      error: (e) => console.log("Error: ", e),
-      complete: () => console.log("Success"),
-    });
   }
 
   cancelRegisterMode(mode: boolean) {
